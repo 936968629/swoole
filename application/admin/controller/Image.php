4913aeedf412a8df21eaf1d5ace7fc10338c87b5
936968629 <936968629@qq.com>
2018-07-22 15:09:19 +0800

    image

---
+++ application/admin/controller/Image.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/22 0022
+ * Time: 15:08
+ */
+
+namespace app\admin\controller;
+
+class Image
+{
+    public function index()
+    {
+
+    }
+}
