f6d706b9003dfa3b4bfe6ed7c68d5812865981c1
936968629 <936968629@qq.com>
2018-07-22 22:02:42 +0800

    upload

--- application/admin/controller/Image.php
+++ application/admin/controller/Image.php
@@ -8,13 +8,21 @@
 
 namespace app\admin\controller;
 
+use app\common\lib\Util;
+
 class Image
 {
     public function index()
     {
-
         $file = request()->file('file');
         $info = $file->move('../public/static/upload');
-        var_dump($info);
+        if ($info) {
+            $data = [
+                'image' => config('live.host')."/".$info->getSaveName(),
+            ];
+            return Util::show(config('code.success'), 'OK', $data);
+        } else {
+            return Util::show(config('code.error'), 'error');
+        }
     }
 }
