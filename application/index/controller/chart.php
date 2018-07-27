b07c45bfbab1930733d9d82e5bf2e456491ad2a8
936968629 <936968629@qq.com>
2018-07-27 23:07:15 +0800

    chart

---
+++ application/index/controller/chart.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/27 0027
+ * Time: 23:05
+ */
+
+namespace app\index\controller;
+
+
+class chart
+{
+    public function index()
+    {
+        
+    }
+}
---
+++ public/static/live/js/chart-push.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2018/7/27 0027.
+ */
+$(function () {
+   $('#discuss').keydown(function (event) {
+        if (event.keyCode == 13) {
+            var text = $(this).val();
+            var url = "http://118.24.112.162:8811/s=index/chart/index";
+            var data = {
+                'content':text,
+                'game_id':1
+            };
+            $.post(url,data,(result)=>{
+
+            },'json');
+        }
+   })
+});
