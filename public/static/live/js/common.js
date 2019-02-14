08d5e357c9fbe412a563616c6a607805c788d1b2
wenjunlin <wenjunlin@100tal.com>
2019-02-14 17:36:13 +0800

    常量读取

--- public/static/live/js/common.js
+++ public/static/live/js/common.js
@@ -2,12 +2,20 @@
  * Created by Administrator on 2018/7/29 0029.
  */
 
-$(function() {
-    var httpUrl = 'http://118.24.112.162:8811/';
-    var wsUrl = 'ws://118.24.112.162:8811';
 
-})
+var httpUrl = 'http://118.24.112.162:8811/';
+var wsUrl = 'ws://118.24.112.162:8811';
 
+
+function getHttpUrl()
+{
+	return httpUrl;
+}
+
+function getWsUrl()
+{
+	return wsUrl
+}
 
 
 
