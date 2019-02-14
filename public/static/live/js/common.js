7ebda37b90ec095a6dea16d259d22c9bdb5a78fa
wenjunlin <wenjunlin@100tal.com>
2019-02-14 17:21:42 +0800

    添加配置js

---
+++ public/static/live/js/common.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2018/7/29 0029.
+ */
+
+$(function() {
+    var httpUrl = 'http://118.24.112.162:8811/';
+    var wsUrl = 'ws://118.24.112.162:8811';
+
+})
+
+
+
+
