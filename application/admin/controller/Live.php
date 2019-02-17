9f92abd23e0df92de400c2657009b29561acb208
wenjunlin <wenjunlin@100tal.com>
2019-02-17 18:59:18 +0800

    消息推送

--- application/admin/controller/Live.php
+++ application/admin/controller/Live.php
@@ -2,6 +2,7 @@
 namespace app\admin\controller;
 
 use app\common\lib\redis\Predis;
+use app\common\lib\Util;
 
 class Live
 {
@@ -12,10 +13,36 @@ class Live
     {
         //赛况入库
         //push到直播页面  获取连接的用户
+        if (empty($_GET)) {
+            return Util::show(config('code.error'), 'error');
+        }
+        //查询数据库
+        $teams = [
+            1 => [
+                'name' => '马刺',
+                'logo' => '/live/imgs/team1.png'
+            ],
+            4 => [
+                'name' => '火箭',
+                'logo' => '/live/imgs/team2.png'
+            ]
+        ];
+
+        $data = [
+            'type' => intval($_GET['type']),
+            'title' => !empty($teams[$_GET['team_id']]['name']) ?$teams[$_GET['team_id']]['name'] : '直播员',
+            'logo' => !empty($teams[$_GET['team_id']]['logo']) ?$teams[$_GET['team_id']]['logo'] : '',
+            'content' => !empty($_GET['content']) ? $_GET['content'] : '',
+            'image' => !empty($_GET['image']) ? $_GET['image'] : '',
+        ];
+//        $taskData = [
+//            'method' => 'pushLive',
+//            'data' => $data
+//        ];
         $clients = Predis::getInstance()->sMembers(config('redis.live_game_key'));
 
         foreach ($clients as $fd) {
-            $_POST['http_server']->push($fd, 'hello-push-data11');
+            $_POST['http_server']->push($fd, json_encode($data));
         }
 
 
