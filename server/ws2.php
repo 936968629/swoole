56f13b8262548461fc7cd853ae030088f5fb5aa2
wenjunlin <wenjunlin@100tal.com>
2019-02-21 13:18:19 +0800

    新增端口

--- server/ws2.php
+++ server/ws2.php
@@ -28,7 +28,7 @@ class Ws {
 
         $this->ws = new swoole_websocket_server(self::HOST, self::PORT);
 
-//        $this->ws->listen(self::HOST, self::CHART_PORT, SWOOLE_SOCK_TCP);
+        $this->ws->listen(self::HOST, self::CHART_PORT, SWOOLE_SOCK_TCP);
 
         $this->ws->set(
             [
