5024a5f9c1461061f9865242de593180c211fdeb
wenjunlin <wenjunlin@100tal.com>
2019-02-23 15:14:24 +0800

    shell服务监控

--- script/monitor/server.php
+++ script/monitor/server.php
@@ -13,10 +13,13 @@ class Server {
     public function port()
     {
 
-        $shell = "netstat -anp | grep ".self::PORT;
+        $shell = "netstat -anp 2>/dev/null | grep ".self::PORT. " LISTEN | wc -l";
 
         $result = shell_exec($shell);
-        echo $result;
+        if ($result != 1) {
+            //发送报警 短信、邮件
+            echo date("Ymd H:i:s")."error".PHP_EOL;
+        }
     }
 }
 
